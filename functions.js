175b2d2393b083606178b5af246f7bc1e9ffd440
klintat <98770096+klintat@users.noreply.github.com>
2024-10-30 18:35:11 +0200

    Created test and function for reversing string

--- functions.js
+++ functions.js
@@ -1,7 +1,11 @@
-function capitalize(string) {
-    if(typeof string !== 'string')
-        throw new TypeError('Input must be a string!');
-    return string[0].toUpperCase + string.slice(1);
+export function capitalize (str) {
+	if (typeof str !== 'string')
+		throw new TypeError('Input must be a string!');
+	return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-module.exports = capitalize;
+export function reversString(str) {
+    if (typeof str !== 'string')
+		throw new TypeError('Input must be a string!');
+    return str.split('').reverse().join('');
+}
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const capitalize = require('./functions.js')
+import { capitalize, reversString } from './functions'
 
 describe('capitalize', () => {
     test('return string with first character capitalized', () => {
@@ -6,5 +6,31 @@ describe('capitalize', () => {
         expect(capitalize('world')).toBe('World');
         expect(capitalize('poro')).toBe('Poro');
         }
-    )}
+    )},
+    test('return sentence with first letter capitalized', () => {
+        expect(capitalize('i need to write tests')).toBe('I need to write tests');
+    }),
+    test('throw an error if invalid input', () => {
+        expect(() => capitalize(null)).toThrow();
+        expect(() => capitalize(0)).toThrow();
+        expect(() => capitalize()).toThrow();
+    })
 )
+
+describe('reverseString', () => {
+    test('reverse given string', () => {
+        expect(reversString('dog')).toBe('god');
+        expect(reversString('cat')).toBe('tac');
+        expect(reversString('string')).toBe('gnirts');
+    }),
+    test('reverse given sentence', () => {
+        expect(reversString('dog bark')).toBe('krab god');
+        expect(reversString('cat sat')).toBe('tas tac');
+        expect(reversString('string reverse')).toBe('esrever gnirts');
+    }),
+    test('throw an error if invalid input', () => {
+        expect(() => capitalize(null)).toThrow();
+        expect(() => capitalize(0)).toThrow();
+        expect(() => capitalize()).toThrow();
+    })
+})
